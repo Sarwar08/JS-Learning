6e359ebb5744d0c1ac9ed0d76afc2563ca7e92ba
Sarwar08 <83556091+Sarwar08@users.noreply.github.com>
2023-12-28 20:30:28 +0000

    Conversion operation added

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+score = null
+score = undefined
+score = true
+score = "sarwar"
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber))
+
+console.log(valueInNumber)
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
